f2140ab377935b2a8e521c32e5e4aa28b9e1ea0e
Ricardo Ortiz <75132128+RicardoOVE@users.noreply.github.com>
2022-10-04 21:39:19 -0500

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,28 +4,46 @@ const Form = () => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [age, setAge] = useState('');
+    const [hizoSubmit, setHizoSubmit] = useState(false);
 
-    const crearUsuario = e => {
-        e.preventDefaul(); // Prevents the normal behavior of the form's submit
-        const nuevoUsuario = {firstName, lastName, age};
-        console.log('New record', nuevoUsuario);
+    const createUser = e => {
+        e.preventDefault(); // Prevents the normal behavior of the form's submit
+        const newUser = {firstName, lastName, age};
+        // const newUser = {firstName: firstName, lastName: lastName, age: age};
+        console.log('New record', newUser);
+
+        setFirstName('');
+        setLastName('');
+        setAge('');
+        setHizoSubmit(true);
+    }
+
+    const mensaje = () =>{
+        if(!hizoSubmit){
+            return 'Por favor ingrese todos tus datos';
+        } else {
+            return 'Gracias por registrarse!'
+        }
     }
 
     return (
-        <form onSubmit={crearUsuario}>
+        <form onSubmit={createUser}>
             <div className="form-group">
                 <label>Name: </label>
-                <input className="form-control"  onChange={(e) => setFirstName(e.target.value)} />
+                <input className="form-control"  onChange={(e) => setFirstName(e.target.value)} value={firstName}/>
             </div>
             <div className="form-group">
                 <label>Last Name: </label>
-                <input className="form-control" onChange={(e) => setLastName(e.target.value)}/>
+                <input className="form-control" onChange={(e) => setLastName(e.target.value)} value={lastName}/>
             </div>
             <div className="form-group">
                 <label>Age: </label>
-                <input className="form-control" onChange={(e) => setAge(e.target.value)}/>
+                <input className="form-control" onChange={(e) => setAge(e.target.value)} value={age}/>
             </div>
             <input type='submit' className="btn btn-dark w-25" value='create user' />
+
+            {mensaje()}
+
         </form>
     )
 }
