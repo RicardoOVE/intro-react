7bd642b77d90dbd995e64603bbe6b91aae7428c4
Ricardo Ortiz <75132128+RicardoOVE@users.noreply.github.com>
2022-10-04 19:34:17 -0500

    Create FirstComponentFunctional.js

---
+++ src/components/FirstComponentFunctional/FirstComponentFunctional.js
@@ -0,0 +1,11 @@
+const FirstComponentFunctional = () => {
+
+    return (
+        <div>
+            <h2>Primer componente funcional</h2>
+        </div>
+    )
+
+}
+
+export default FirstComponentFunctional
